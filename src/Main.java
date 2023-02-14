f830b8b7e65cb3d48631a4a504bdcfc68fdfc74e
tomasposp <zelvicka52525@seznam.cz>
2023-02-14 10:26:36 +0100

    Hello 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+    System.out.println("Hello 2");
+
     }
 }
